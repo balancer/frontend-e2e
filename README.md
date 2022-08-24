69620f59c31c626cc0262abe27cac7a2ff51461d
sssmi <simeon.kerkola@gmail.com>
2022-08-24 08:19:03 +0300

    Add swap test and secret test wallet

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Balancer Frontend testing with Synpress
+
+**Test wallet address:** `0xc9a9532a0d0ca26174caa328c5972a4bdc42c93b`
+
+## Setup
+
+1. Create file `.env` to project root and add these required env variables:
+
+```sh
+SECRET_WORDS="word1 word2 ... ... etc"
+NETWORK_NAME=goerli
+```
+
+2. Start the frontend app using the same network you defined in `.env` file
--- synpress/support/index.ts
+++ synpress/support/index.ts
@@ -1,6 +1,20 @@
+/// <reference types="cypress" />
 import './commands';
 import '@synthetixio/synpress/support';
 import addContext from 'mochawesome/addContext';
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Cypress {
+    interface Chainable {
+      /**
+       * Custom command to click Connect Wallet Button in Nav and
+       * select Metamask
+       */
+      connectWallet(): void;
+    }
+  }
+}
 
 Cypress.on('test:after:run', (test, runnable) => {
   if (test.state === 'failed') {
@@ -20,4 +34,15 @@ Cypress.on('test:after:run', (test, runnable) => {
     addContext({ test }, screenshot);
     addContext({ test }, video);
   }
+});
+
+// find "Connect Wallet" button and click it
+Cypress.Commands.add('connectWallet', () => {
+  cy.get('nav')
+    .within(() => {
+      cy.findByRole('button', { name: /Connect Wallet/i }).click();
+    })
+    .then(() => {
+      cy.findByRole('button', { name: /Metamask/i }).click();
+    });
 });
