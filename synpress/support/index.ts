46c5612c267823bf081e5dbd423ea37be253037f
sssmi <simeon.kerkola@gmail.com>
2022-08-25 13:25:29 +0300

    Always add screenshot and video to test report

--- synpress/support/index.ts
+++ synpress/support/index.ts
@@ -3,23 +3,23 @@ import './commands';
 import '@synthetixio/synpress/support';
 import addContext from 'mochawesome/addContext';
 
-// Add screenshots and video to test report
+// Add screenshots and video to test report if test failed
 Cypress.on('test:after:run', (test, runnable) => {
-  if (test.state === 'failed') {
-    const imageUrl = `assets/screenshots/${Cypress.spec.name}/${runnable.parent.title} -- ${test.title} (failed).png`;
-    const videoUrl = `assets/videos/${Cypress.spec.name}.mp4`;
+  // if (test.state === 'failed') {
+  const imageUrl = `assets/screenshots/${Cypress.spec.name}/${runnable.parent.title} -- ${test.title} (failed).png`;
+  const videoUrl = `assets/videos/${Cypress.spec.name}.mp4`;
 
-    const screenshot = {
-      title: 'Screenshot',
-      value: imageUrl,
-    };
+  const screenshot = {
+    title: 'Screenshot',
+    value: imageUrl,
+  };
 
-    const video = {
-      title: 'Video',
-      value: videoUrl,
-    };
+  const video = {
+    title: 'Video',
+    value: videoUrl,
+  };
 
-    addContext({ test }, screenshot);
-    addContext({ test }, video);
-  }
+  addContext({ test }, screenshot);
+  addContext({ test }, video);
+  // }
 });
