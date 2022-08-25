63a9628909a7bf1aa97e30b33cf51df692677801
sssmi <simeon.kerkola@gmail.com>
2022-08-25 11:26:07 +0300

    Remove connect wallet helper

--- synpress/support/index.ts
+++ synpress/support/index.ts
@@ -3,19 +3,6 @@ import './commands';
 import '@synthetixio/synpress/support';
 import addContext from 'mochawesome/addContext';
 
-declare global {
-  // eslint-disable-next-line @typescript-eslint/no-namespace
-  namespace Cypress {
-    interface Chainable {
-      /**
-       * Custom command to click Connect Wallet Button in Nav and
-       * select Metamask
-       */
-      connectWallet(): void;
-    }
-  }
-}
-
 Cypress.on('test:after:run', (test, runnable) => {
   if (test.state === 'failed') {
     const imageUrl = `assets/screenshots/${Cypress.spec.name}/${runnable.parent.title} -- ${test.title} (failed).png`;
@@ -34,15 +21,4 @@ Cypress.on('test:after:run', (test, runnable) => {
     addContext({ test }, screenshot);
     addContext({ test }, video);
   }
-});
-
-// find "Connect Wallet" button and click it
-Cypress.Commands.add('connectWallet', () => {
-  cy.get('nav')
-    .within(() => {
-      cy.findByRole('button', { name: /Connect Wallet/i }).click();
-    })
-    .then(() => {
-      cy.findByRole('button', { name: /Metamask/i }).click();
-    });
 });
