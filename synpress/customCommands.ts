ba311b763756b9368351f06ea24d0494888910d9
sssmi <simeon.kerkola@gmail.com>
2022-08-25 18:54:39 +0300

    Remove timeout

--- synpress/customCommands.ts
+++ synpress/customCommands.ts
@@ -4,31 +4,35 @@
  * Add custom commands here instead and import them in the test you need
  */
 
-// find "Connect Wallet" button and click it
 export function connectWallet() {
-  return cy
-    .get('nav')
-    .within(() => {
-      cy.findByRole('button', { name: /Connect Wallet/i }).click();
-    })
-    .then(() => {
-      cy.findByRole('button', { name: /Metamask/i }).click();
-      // eslint-disable-next-line cypress/no-unnecessary-waiting
-      cy.wait(1000);
-      cy.window()
-        .then((win: any) =>
-          !win.ethereum ? [] : win.ethereum.request({ method: 'eth_accounts' })
-        )
-        .then(accounts => {
-          if (!accounts.length) {
-            cy.switchToMetamaskWindow();
-            cy.acceptMetamaskAccess(undefined).should('be.true');
+  return (
+    cy
+      // find "Connect Wallet" button in nav bar and click it
+      .get('nav')
+      .within(() => {
+        cy.findByRole('button', { name: /Connect Wallet/i }).click();
+      })
+      .then(() => {
+        // Select metamask from modal
+        cy.findByRole('button', { name: /Metamask/i }).click();
 
-            // Confirming not needed?
-            // cy.confirmMetamaskSignatureRequest();
+        cy.window()
+          .then((win: any) =>
+            !win.ethereum
+              ? []
+              : win.ethereum.request({ method: 'eth_accounts' })
+          )
+          .then(accounts => {
+            if (!accounts.length) {
+              cy.switchToMetamaskWindow();
+              cy.acceptMetamaskAccess(undefined).should('be.true');
 
-            cy.switchToCypressWindow();
-          }
-        });
-    });
+              // Confirming not needed?
+              // cy.confirmMetamaskSignatureRequest();
+
+              cy.switchToCypressWindow();
+            }
+          });
+      })
+  );
 }
