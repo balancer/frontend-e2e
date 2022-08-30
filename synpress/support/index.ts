381d366985c47fc92f5f9c90dba3b468e2abc297
sssmi <simeon.kerkola@gmail.com>
2022-08-30 16:16:43 +0300

    Clean

--- synpress/support/index.ts
+++ synpress/support/index.ts
@@ -21,30 +21,3 @@ Cypress.on('test:after:run', (test, runnable) => {
   addContext({ test }, screenshot);
   addContext({ test }, video);
 });
-
-// before(async () => {
-//   if (!Cypress.env('SKIP_METAMASK_SETUP')) {
-//     // await cy.setupMetamask();
-//   }
-// });
-
-// Cypress.Commands.overwrite(
-//   'setupMetamask',
-//   (secretWordsOrPrivateKey, network, password = 'Tester@1234') => {
-//     cy.task('setupMetamask', {
-//       secretWordsOrPrivateKey,
-//       network,
-//       password,
-//     });
-//     cy.wait(1000);
-//     cy.switchToMetamaskWindow();
-//     const accountNumber = 3;
-
-//     for (let i = 0; i <= accountNumber; i++) {
-//       cy.createMetamaskAccount(undefined);
-//     }
-//     cy.acceptMetamaskAccess(undefined).should('be.true');
-//     cy.switchMetamaskAccount(accountNumber);
-//     cy.switchToCypressWindow();
-//   }
-// );
