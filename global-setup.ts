d9718023b031de7324a7933625e3c91d1d6cecaa
sssmi <simeon.kerkola@gmail.com>
2023-02-08 20:44:58 +0200

    Run dappwright headless in CI

--- global-setup.ts
+++ global-setup.ts
@@ -3,11 +3,13 @@ import dappwright, { MetaMaskWallet } from '@tenkeylabs/dappwright';
 // import playwright from 'playwright';
 
 async function globalSetup(config: FullConfig) {
+  console.log('check variables', process.env.CI);
   const [metamask, page, context] = await dappwright.bootstrap('', {
     wallet: 'metamask',
     password: 'testingtesting',
     seed: process.env.SECRET_WORDS,
     version: MetaMaskWallet.recommendedVersion,
+    headless: !!process.env.CI,
   });
 
   // Add Hardhet network
