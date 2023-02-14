ab6feaf45a1a41574eb899d8ab5e1a6599a818e2
sssmi <simeon.kerkola@gmail.com>
2023-02-14 12:16:03 +0200

    increase timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,10 +20,10 @@ const config: PlaywrightTestConfig = {
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 5000,
+    timeout: 10000,
   },
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
 
